35b2a60e5eeedd79269818ae8eec1d5997432616
Pablo RUTH <contact@pablo-ruth.fr>
2018-10-11 18:22:23 +0200

    initial commit

---
+++ provider.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/Ouest-France/goitop"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+// Provider returns a terraform.ResourceProvider.
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"address": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "itop server address",
+			},
+			"user": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "itop username",
+			},
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "itop password",
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"itop_virtual_machine": resourceVirtualMachine(),
+		},
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	return goitop.NewClient(
+		d.Get("address").(string),
+		d.Get("user").(string),
+		d.Get("password").(string),
+	), nil
+}
---
+++ resource_itop_virtual_machine.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"github.com/Ouest-France/goitop"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceVirtualMachine() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceVirtualMachineCreate,
+		Read:   resourceVirtualMachineRead,
+		Update: resourceVirtualMachineUpdate,
+		Delete: resourceVirtualMachineDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Virtual Machine name",
+			},
+			"org_id": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Organization ID",
+			},
+			"env_id": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Environment ID",
+			},
+		},
+	}
+}
+
+func resourceVirtualMachineCreate(d *schema.ResourceData, m interface{}) error {
+	client := m.(*goitop.Client)
+
+	id, err := client.CreateVM(
+		d.Get("name").(string),
+		d.Get("org_id").(string),
+		d.Get("env_id").(string),
+	)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(id)
+	return nil
+}
+
+func resourceVirtualMachineRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(*goitop.Client)
+
+	vm, err := client.GetVM(d.Id())
+	if err != nil {
+		return err
+	}
+
+	d.SetId(vm.ID)
+
+	d.Set("name", vm.Name)
+	d.Set("org_id", vm.OrgID)
+	d.Set("env_id", vm.EnvID)
+
+	return nil
+}
+
+func resourceVirtualMachineUpdate(d *schema.ResourceData, m interface{}) error {
+	client := m.(*goitop.Client)
+
+	err := client.UpdateVM(
+		d.Id(),
+		d.Get("name").(string),
+		d.Get("org_id").(string),
+		d.Get("env_id").(string),
+	)
+	return err
+}
+
+func resourceVirtualMachineDelete(d *schema.ResourceData, m interface{}) error {
+	client := m.(*goitop.Client)
+
+	return client.DeleteVM(d.Id())
+}
